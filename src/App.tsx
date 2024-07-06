15fd78386237321ba61f91bac9882b69a6071a0a
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2024-07-06 18:26:40 +0200

    added About page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Start from "./Pages/Start";
 import PageNotFound from "./Pages/PageNotFound";
+import About from "./Pages/About";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Start/>
+    },
+    {
+        path: "/about",
+        element: <About/>
     },
     {
         path: "*",
---
+++ src/Pages/About.tsx
@@ -0,0 +1,29 @@
+import { ReactElement } from "react";
+import Header from "../Components/Header";
+
+const About = (): ReactElement => {
+  return (
+    <div>
+      <Header title={"About Page"} />
+      <section className="container-fluid">
+        <h1>About Us</h1>
+        <p>This is the about page</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim cum
+          ullam accusantium vero, esse dolorem similique magnam rerum,
+          aspernatur ipsa itaque, excepturi ratione voluptas. Hic vero
+          perferendis et iusto minus tempora. Tenetur magnam accusamus et,
+          error, numquam, itaque omnis possimus alias voluptas magni eligendi
+          mollitia officiis dicta! Quod corporis, porro harum dicta architecto
+          autem quibusdam debitis consequatur. Recusandae laborum repellendus
+          reprehenderit et tempore cum minus, saepe deleniti hic obcaecati aut
+          fuga tempora eos quasi enim omnis ex? In natus aut impedit minus amet
+          optio quasi enim mollitia, molestias voluptate veniam ratione dolorem,
+          neque laboriosam eos error delectus ab. Perspiciatis, delectus.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default About;
